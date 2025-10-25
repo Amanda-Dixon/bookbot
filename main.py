1f94f182fcac1135eb3ec9fb7e70ecbbcfccd98d
sweetnsoya <sweetnsoya.ad@gmail.com>
2025-10-25 23:04:20 +0100

    Added a function to count the number of characters (all lowercase, including spaces and special characters).

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
+## to import the text from a filepath
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
@@ -7,10 +8,14 @@ def get_book_text(filepath):
 
 def main():
 
+## to count number of words
     filepath = "./books/frankenstein.txt"
     text = get_book_text(filepath)
     num_words = get_num_words(text)
     print (f"Found {num_words} total words")
 
+## to count number of times characters appear
+    num_characters = get_num_chars(text)
+    print(num_characters)
 
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,19 @@ def get_num_words(text):
     for word in words:
           n +=1
     return n
+
+## Function that counts the number of times a character appears in a text
+## (Including special characters and spaces)
+
+def get_num_chars(text):
+     num_chars = {}
+     lower_case = text.lower()           
+     for char in lower_case:
+            if char in num_chars:
+                  num_chars[char] += 1                
+            else:
+                  num_chars[char] = 1      
+     return num_chars
+
+
+
