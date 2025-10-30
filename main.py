f1ce30647c35b680b5c8c6cec8791ae4df7cdf06
sweetnsoya <sweetnsoya.ad@gmail.com>
2025-10-30 11:56:34 +0000

    Altered to accept other books using sys.argv, with an error if this is not included when running main.py

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
 from stats import get_num_words, get_num_chars, sorted_chars
+import sys
 
 ## to import the text from a filepath
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
-
-def main():
-
-## to count number of words
-    filepath = "./books/frankenstein.txt"
-    text = get_book_text(filepath)
-    num_words = get_num_words(text)
-
-
-## to count number of times characters appear
-    num_characters = get_num_chars(text)
-#    print(num_characters)
-
-## to create an ordered list of dictionaries of characters and their values
-    sorted_character_list = sorted_chars(num_characters)
-
-## to print beginning of BOOKBOT output
- 
+def print_report(filepath, num_words, sorted_character_list):
+    ## to print beginning of BOOKBOT output
     print ("============ BOOKBOT ============")
     print (f"Analyzing book found at {filepath}...")
     print ("----------- Word Count ----------")
@@ -36,7 +21,26 @@ def main():
             print (f"{entry["char"]}: {entry["num"]}")
 
 ## to print end of BOOKBOT output
-
     print ("============= END ===============")
+
+    
+def main():
+
+## error message if two sys.argv entries aren't included
+    if len(sys.argv) != 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+## to count number of words
+#    filepath = "./books/frankenstein.txt"
+    filepath = sys.argv[1]
+    text = get_book_text(filepath)
+    num_words = get_num_words(text)
+## to count number of times characters appear
+    num_characters = get_num_chars(text)
+## to create an ordered list of dictionaries of characters and their values
+    sorted_character_list = sorted_chars(num_characters)
+## to print the report   
+    print_report(filepath, num_words, sorted_character_list)
 
 main()
